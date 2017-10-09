1c6e172787932789defe317292fd06505afc3ad1
Unknown <lucacasonato@yahoo.com>
2017-10-09 11:43:39 +0200

    Added all go files

---
+++ api/websocket/main.go
@@ -0,0 +1,81 @@
+package websocket
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
+)
+
+type MessageReceived interface {
+	MessageReceived(p []byte)
+}
+
+type settings struct {
+	MessageReceived
+}
+
+//Settings
+var Settings = settings{messageReceived}
+
+func messageReceived(p []byte) {
+
+}
+
+//MessageReciver
+func (s *settings) MessageReceiver(c *websocket.Conn) {
+	for {
+		mt, message, err := c.ReadMessage()
+
+		if err != nil {
+			fmt.Println("read:", err)
+			break
+		}
+		s.MessageReceived(message)
+		err = c.WriteMessage(mt, message)
+		if err != nil {
+			log.Println("write:", err)
+			break
+		}
+	}
+}
+
+type rect struct {
+	width, height int
+}
+
+func (r *rect) area() int {
+	return r.width * r.height
+}
+
+var upgrader = websocket.Upgrader{} // use default options
+
+func init() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", wsUpgrader)
+	http.Handle("/", r)
+	fmt.Println("[WebsocketAPI] Websocket API is initialized.")
+	re := rect{width: 10, height: 5}
+}
+
+func wsUpgrader(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+	defer c.Close()
+	Settings.MessageReceiver(c)
+}
+
+//HelloWorld is a test function!
+func HelloWorld(hello string) {
+	fmt.Println(hello)
+}
+
+//ParseMessage
+func ParseMessage() {
+
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/creativeguy2013/go_websocketapi/api/websocket"
+)
+
+func main() {
+	websocket.HelloWorld("hello")
+}
